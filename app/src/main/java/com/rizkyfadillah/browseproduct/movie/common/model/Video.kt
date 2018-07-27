595e7498d28e19e160b11a658b6322bc146793e2
Rizky Fadillah <rizkyfadillah1994@gmail.com>
2018-07-27 22:03:08 +0700

    push

--- app/src/main/java/com/rizkyfadillah/browseproduct/movie/common/model/Video.kt
+++
@@ -1,9 +0,0 @@
-package com.rizkyfadillah.browseproduct.movie.common.model
-
-import com.google.gson.annotations.SerializedName
-
-/**
- * Created by Rizky on 18/04/18.
- */
-
-class Video (val id: String, thumbnail: String, val title: String)
